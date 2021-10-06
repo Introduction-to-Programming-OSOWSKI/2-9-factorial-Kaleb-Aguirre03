ebce948a5ca87fdd0a0b3b3d3fee730e98b5bf16
Kaleb-Aguirre03 <820930@mystma.org>
2021-10-06 09:03:06 -0500

    #1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#WRITE YOUR CODE IN THIS FILE
+#Factorial: 5! = 5 * 4 * 3 * 2 * 1
+
+#Define a variable that does a factorial of some number x
+def factorial(x):
+    
+    total = 1
+    
+    for i in range(1, x + 1):
+        total = total * i 
+
+    return total 
+
+#print the funtion in order to test it
+print (factorial(1000))
